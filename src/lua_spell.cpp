ba1f17f7d981381dd7b8b63615b362abe767fdae
Nyel <41634901+Nyedson@users.noreply.github.com>
2025-04-07 17:56:38 -0300

    Update lua_spell.cpp

--- src/lua_spell.cpp
+++ src/lua_spell.cpp
@@ -25,7 +25,7 @@ int LuaScriptInterface::luaSpellCreate(lua_State* L)
 	SpellType_t spellType = SPELL_UNDEFINED;
 
 	if (isNumber(L, 2)) {
-		int32_t id = getNumber<int32_t>(L, 2);
+		int16_t id = getNumber<int16_t>(L, 2);
 		RuneSpell* rune = g_spells->getRuneSpell(id);
 
 		if (rune) {
@@ -171,7 +171,7 @@ int LuaScriptInterface::luaSpellId(lua_State* L)
 		if (lua_gettop(L) == 1) {
 			lua_pushnumber(L, spell->getId());
 		} else {
-			spell->setId(getNumber<uint8_t>(L, 2));
+			spell->setId(getNumber<uint16_t>(L, 2));
 			pushBoolean(L, true);
 		}
 	} else {
